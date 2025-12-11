897e486a1ff6587f8af39570be5415c8373c137b
Clarris <clarris00@gmail.com>
2025-12-11 13:15:23 +0700

    more detail on FormLogin

--- RunningWinForm/Forms/FormLogin.cs
+++ RunningWinForm/Forms/FormLogin.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using RunningWinForm.Data.Repositories;
+using System.Windows.Media.Media3D;
+using RunningWinForm.Models;
 
 namespace RunningWinForm
 {
@@ -26,12 +28,36 @@ namespace RunningWinForm
             InitializeComponent();
         }
 
+        public User LoggedInUser { get; private set; }
+
         private void btnLogin_Click(object sender, EventArgs e)
         {
             String username = txtUsername.Text;
             String password = txtPassword.Text;
-            
-            var user = 
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập đủ thông tin!", "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var user = _userRepository.GetUserAndPassword(username, password);
+
+            if(user == null)
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu!", "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUsername.Focus();
+            }
+            else
+            {
+                LoggedInUser = user;
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
         }
     }
 }
